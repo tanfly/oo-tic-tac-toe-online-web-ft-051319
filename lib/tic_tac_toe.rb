b1e902adde2d872ba17f2d80fdc4a7eab6115e6e
Stella McEwen <mcewen.stella@gmail.com>
2019-05-26 02:25:18 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -62,7 +62,6 @@ def turn
     puts "Please enter a number 1-9:"
     user_input = gets.strip
     index = input_to_index(user_input)
-    token = current_player
     until valid_move?(index)
     puts "Please enter a number 1-9:"
     user_input = gets.strip
