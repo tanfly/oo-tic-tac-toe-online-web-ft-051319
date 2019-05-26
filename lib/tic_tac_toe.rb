736484fb261d93d7cf1b37a710ff5ac71c2767f8
Stella McEwen <mcewen.stella@gmail.com>
2019-05-26 02:23:52 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -63,10 +63,11 @@ def turn
     user_input = gets.strip
     index = input_to_index(user_input)
     token = current_player
-    if valid_move?(index)
-      move(index, token)
-      display_board
-    else 
+    until valid_move?(index)
+    puts "Please enter a number 1-9:"
+    user_input = gets.strip
+    index = input_to_index(user_input)
+    end 
       turn
     end
 end
